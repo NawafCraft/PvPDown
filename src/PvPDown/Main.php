7f91774d464772fac15c26bfc0a54a5d655b121e
Adnan <pmservermcc@gmail.com>
2016-07-13 12:13:14 +0300

    Update Main.php

--- src/PvPDown/Main.php
+++ src/PvPDown/Main.php
@@ -10,9 +10,31 @@ use pocketmine\scheduler\PluginTask;
 use pocketmine\math\Vector3;
 
 class Main extends PluginBase implements Listener {
+	
+	public $players = array();
+    
     public function onEnable() {
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+         $this->getServer()->getPluginManager()->registerEvents($this , $this);
+         $this->getServer()->getScheduler()->scheduleRepeatingTask(new Time($this), 60);
+        $this->getLogger()->info(C::GREEN."--------------------------------------------------------------");
+        $this->getLogger()->info(C::GOLD."Has Been Endable , By : ".C::AQUA."Nawaf_Craft1b");
+        $this->getLogger()->info(C::GREEN."--------------------------------------------------------------");
+       }
+       public function addPlayer(Player $player) {
+           foreach($this->players as $name => $plr){
+            $plr->sendMessage("The player ".$player->getName()." has joined the match!");
+        }
+        $this->players[strtolower($player->getName())] = $player;
+       }
+       public function removePlayer(Player $player){
+        if(isset($this->players[$player])){
+            unset($this->players[$player]);
     }
+  }
+   public function isPlaying(Player $player){
+        return isset($this->players[strtolower($player->getName())]);
+    }
+	
     // starting now code :$
 }
 class Time extends PluginTask {
